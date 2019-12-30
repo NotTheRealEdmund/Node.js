7a5efd982b60e6e864ac34891e5c128a1bf28154
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2019-12-30 15:43:01 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const x = require('./tutorial');
+console.log(x);
+console.log(x.sum(1,1)); 
+console.log(x.diff(1,1)); 
---
+++ tutorial.js
@@ -0,0 +1,5 @@
+const sum = (num1,num2) => num1 + num2;
+module.exports.sum = sum;
+
+const diff = (num1,num2) => num1 - num2;
+module.exports.diff = diff;
